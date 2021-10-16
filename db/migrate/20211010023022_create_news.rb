a5843fc9b830a7830abbb4c10a48f54e8dcb4b58
y-n-h-y <upboshi.bigboy@gmail.com>
2021-10-16 16:01:36 +0000

    [add]管理者ページニュース

--- db/migrate/20211010023022_create_news.rb
+++ db/migrate/20211010023022_create_news.rb
@@ -2,6 +2,7 @@ class CreateNews < ActiveRecord::Migration[5.2]
   def change
     create_table :news do |t|
       t.integer :shop_id
+      t.integer :genre
       t.string :title
       t.string :image_id
       t.text :content
