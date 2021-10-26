ef7d54447680ab4df45887c2d8f9b5002d3501b5
y-n-h-y <upboshi.bigboy@gmail.com>
2021-10-26 15:00:19 +0000

    [add]ユーザープロフィール編集

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -4,19 +4,36 @@ class Public::CustomersController < ApplicationController
     @current_like = @like.where(shop_id: [:shop_id])
   end
 
-  def show
+  def top
     @customer = current_customer.id
   end
 
+  def show
+    @customer = current_customer
+  end
+
   def edit
+    @customer = Customer.find(params[:id])
   end
 
   def update
+    @customer = Customer.find(params[:id])
+    if @customer.update(customer_params)
+      redirect_to customer_path(@customer)
+    else
+      render :edit
+    end
   end
 
   def confirm
   end
 
   def withdrawal
+  end
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(:nickname, :email, :state, :city)
   end
 end
