9eee16fd2d6a7efafc9b4d0422ea1aef2a22b51f
y-n-h-y <upboshi.bigboy@gmail.com>
2021-11-03 03:05:24 +0000

    [add]お気に入り機能

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -5,6 +5,7 @@ class Customer < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :reviews, dependent: :destroy
+  has_many :favorites, dependent: :destroy
 
     enum state: {
      "都道府県":0,
