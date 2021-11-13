142bdd24ca4c9f55ab4f0d7b0394b7b41110bd78
y-n-h-y <upboshi.bigboy@gmail.com>
2021-11-13 14:07:33 +0000

    [add]管理者編集

--- app/controllers/admin/admins_controller.rb
+++ app/controllers/admin/admins_controller.rb
@@ -4,14 +4,27 @@ class Admin::AdminsController < ApplicationController
   end
 
   def edit
+    @admin = Admin.find(params[:id])
   end
 
   def update
+    @admin = Admin.find(params[:id])
+    if @admin.update(admin_params)
+      redirect_to admin_admin_path(@admin)
+    else
+      render :edit
+    end
   end
 
   def confirm
   end
 
   def withdrawal
+  end
+
+  private
+
+  def admin_params
+    params.require(:admin).permit(:company_name, :name, :name_kana, :email, :phone, :is_active)
   end
 end
