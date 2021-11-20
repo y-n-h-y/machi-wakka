34144d4becec09d7263c8d5da98a245ecc6403fe
y-n-h-y <upboshi.bigboy@gmail.com>
2021-11-20 10:36:02 +0000

    [Fix]javascript

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -86,11 +86,13 @@ $(document).ready(function() {
 });
 
 $(function() {
-  let tabs = $("li").attr(".tab"); // tabのクラスを全て取得し、変数tabsに配列で定義
+  var tabs = $(".tab"); // tabのクラスを全て取得し、変数tabsに配列で定義
+
   $(".tab").on("click", function() { // tabをクリックしたらイベント発火
     $(".active").removeClass("active"); // activeクラスを消す
     $(this).addClass("active"); // クリックした箇所にactiveクラスを追加
     const index = tabs.index(this); // クリックした箇所がタブの何番目か判定し、定数indexとして定義
+    console.log(index)
     $(".area").removeClass("show").eq(index).addClass("show"); // showクラスを消して、contentクラスのindex番目にshowクラスを追加
   })
 })
