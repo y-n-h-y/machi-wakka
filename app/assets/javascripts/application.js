f3b59ea5b522cd1a5ac61bfb5f6cbbb237400926
y-n-h-y <upboshi.bigboy@gmail.com>
2022-01-20 14:39:45 +0000

    css slick

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -79,13 +79,13 @@ $(document).ready(function() {
     autoplay: true,
     slidesToShow: 3,
     responsive: [{
-        breakpoint: 768,
+        breakpoint: 1024,
         settings: {
-          slidesToShow: 3,
+          slidesToShow: 2,
         }
       },
       {
-        breakpoint: 540,
+        breakpoint: 600,
         settings: {
           slidesToShow: 1,
         }
